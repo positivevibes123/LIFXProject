bce7ca17436c75ac4d88cac1c0aa627f0705e961
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2024-06-13 13:29:32 -0400

    Update Test.java
    Removed unnecessary info

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String token = "c6a42da12dd57e5fd2d48326a01bf4e3aee9a2514bdf16eccbab6f725b531f89";
+        String token = "(INSERT TOKEN HERE)";
 
         ClientSingleton.getInstance().init(token);
         //ClientSingleton.getInstance().authenticate();
